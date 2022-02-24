8dd2bae00f34b257a5fbd35ee89ddd4377421db2
DaneKellogg <dane.smith@kellogg.northwestern.edu>
2022-02-24 04:03:41 +0000

    4

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,2 +1,28 @@
 class PlacesController < ApplicationController
+    def index
+        @places = 
+    end
+
+    def place
+        @place = Place.find(params["id"])
+    end
+
+    def newplace
+        @place = Place.new
+    end
+
+    def create
+        @place = Place.new(params["place"])
+        @place.save
+        redirect_to "/places"
+    end
+    
+    def destroy
+        @place = Place.find(params["id"])
+        @place.destroy
+        redirect_to "/places"
+    end
+
+end
+
 end
